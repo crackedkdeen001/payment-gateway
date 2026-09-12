5a342924d7c61f207ea008912cd9ed69e5ce21f9
kamal <kbaiyewu108@gmail.com>
2026-09-12 16:34:57 +0100

    fix: test not importing .env from config

--- src/core/config.py
+++ src/core/config.py
@@ -1,11 +1,23 @@
-from pydantic import Field
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
+# dynamically sets the file path depending on whether the 
+# .env file is read from tests or used normally
+base_dir_name = Path.cwd().name
+if base_dir_name == "tests":
+    env_args = {
+    "env_file" : str(Path.cwd().parent / ".env")
+    }
+else:
+    env_args = {
+        "env_file" : ".env",
+        "env_file_depth" : 3
+    }
+    
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_depth="3")
+    model_config = SettingsConfigDict(**env_args)
     
-    database_url: str = Field(alias="DATABASE_URL")
-    
+    database_url: str
     
 settings = Config()
